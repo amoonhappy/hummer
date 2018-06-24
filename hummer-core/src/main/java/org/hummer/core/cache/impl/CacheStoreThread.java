fa43a290fa765fe3addf7dd3d1da278004d640cf
amoonhappy <152318@qq.com>
2018-06-24 17:35:03 +0800

    optimize cache performance

--- hummer-core/src/main/java/org/hummer/core/cache/impl/CacheStoreThread.java
+++ hummer-core/src/main/java/org/hummer/core/cache/impl/CacheStoreThread.java
@@ -17,36 +17,41 @@ import java.util.stream.Collectors;
 public class CacheStoreThread {
     private final static Logger log = Log4jUtils.getLogger(CacheStoreThread.class);
     private static final RedisService redisService = (RedisService) HummerContainer.getInstance().getBeanFromSpring("redisService");
-//    @Autowired
+    private static final boolean epirationEnabled = CacheManager.isExpirationEnabled();
+    private static final int epirationPeriod = CacheManager.getExpirationPeriod();
+
+    //    @Autowired
 //    RedisService redisService;
 
     public void storeResultToRedis(Object returnValue, Object redisKey, CacheKey cacheKey) {
         new Thread("CacheStoreThread") {
             public void run() {
-                log.debug("entering a new thread:{} on {}", Thread.currentThread().getName(), Thread.currentThread().toString());
-                boolean evictOnAll = cacheKey.evictOnAll();
-                //将结果集放入Redis缓存, 如果返回值为空，也继续存入redis，避免访问数据库
-                if (CacheManager.isExpirationEnabled()) {
-                    redisService.setWithExp(redisKey, CacheManager.getExpirationPeriod(), returnValue);
-                } else {
-                    redisService.set(redisKey, returnValue);
-                }
+                if (redisKey != null && cacheKey != null) {
+                    log.debug("entering a new thread:{} on {}", Thread.currentThread().getName(), Thread.currentThread().toString());
+                    boolean evictOnAll = cacheKey.evictOnAll();
+                    //将结果集放入Redis缓存, 如果返回值为空，也继续存入redis，避免访问数据库
+                    if (epirationEnabled) {
+                        redisService.setWithExp(redisKey, epirationPeriod, returnValue);
+                    } else {
+                        redisService.set(redisKey, returnValue);
+                    }
 
-                //如果不通过Model.Id祛除Cache，并且返回值不为空
-                if (!evictOnAll && returnValue != null) {
-                    //store the rediskey and ids mapping
-                    if (returnValue instanceof IModel) {
-                        String id = ((IModel) returnValue).getId();
-                        CacheManager.registerRedisKeyForId(returnValue.getClass(), id, redisKey);
-                    } else if (returnValue instanceof Collection) {
-                        // 使用jdk1.8中的新Collector实现从Collection中获取对象.方法的值返回一个集合
-                        Set<String> ids = ((Collection<IModel>) returnValue).stream().map(IModel::getId).collect(Collectors.toCollection(TreeSet::new));
-                        Class modelClass = ((Collection<IModel>) returnValue).iterator().next().getClass();
-                        CacheManager.registerRedisKeyForIds(modelClass, ids, redisKey);
-                    } else if (returnValue instanceof Map) {
-                        Set<String> ids = ((Map) returnValue).keySet();
-                        Class modelClass = ((Map) returnValue).values().iterator().next().getClass();
-                        CacheManager.registerRedisKeyForIds(modelClass, ids, redisKey);
+                    //如果不通过Model.Id祛除Cache，并且返回值不为空
+                    if (!evictOnAll && returnValue != null) {
+                        //store the rediskey and ids mapping
+                        if (returnValue instanceof IModel) {
+                            String id = ((IModel) returnValue).getId();
+                            CacheManager.registerRedisKeyForId(returnValue.getClass(), id, redisKey);
+                        } else if (returnValue instanceof Collection) {
+                            // 使用jdk1.8中的新Collector实现从Collection中获取对象.方法的值返回一个集合
+                            Set<String> ids = ((Collection<IModel>) returnValue).stream().map(IModel::getId).collect(Collectors.toCollection(TreeSet::new));
+                            Class modelClass = ((Collection<IModel>) returnValue).iterator().next().getClass();
+                            CacheManager.registerRedisKeyForIds(modelClass, ids, redisKey);
+                        } else if (returnValue instanceof Map) {
+                            Set<String> ids = ((Map) returnValue).keySet();
+                            Class modelClass = ((Map) returnValue).values().iterator().next().getClass();
+                            CacheManager.registerRedisKeyForIds(modelClass, ids, redisKey);
+                        }
                     }
                 }
             }
