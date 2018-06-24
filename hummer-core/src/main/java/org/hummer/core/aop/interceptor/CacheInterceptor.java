fb737f0f977015e9ac97c1c370b0663ac6bac9e4
amoonhappy <152318@qq.com>
2018-06-24 17:08:37 +0800

    bug fix for cache

--- hummer-core/src/main/java/org/hummer/core/aop/interceptor/CacheInterceptor.java
+++ hummer-core/src/main/java/org/hummer/core/aop/interceptor/CacheInterceptor.java
@@ -4,7 +4,10 @@ import org.aopalliance.intercept.MethodInvocation;
 import org.hummer.core.aop.impl.Perl5DynamicMethodInterceptor;
 import org.hummer.core.aop.intf.KeyGenerator;
 import org.hummer.core.cache.annotation.CacheKey;
-import org.hummer.core.cache.impl.*;
+import org.hummer.core.cache.impl.CacheEvaluationContext;
+import org.hummer.core.cache.impl.CacheStoreThread;
+import org.hummer.core.cache.impl.RedisService;
+import org.hummer.core.cache.impl.SimpleKeyGenerator;
 import org.hummer.core.cache.intf.ICacheable;
 import org.hummer.core.container.impl.HummerContainer;
 import org.hummer.core.util.Log4jUtils;
@@ -25,8 +28,11 @@ import java.lang.reflect.Method;
 @SuppressWarnings("all")
 public class CacheInterceptor extends Perl5DynamicMethodInterceptor {
     private static final Logger log = Log4jUtils.getLogger(CacheInterceptor.class);
-    RedisService redisService = (RedisService) HummerContainer.getInstance().getBeanFromSpring("redisService");
-//    @Autowired
+    private static final RedisService redisService = (RedisService) HummerContainer.getInstance().getBeanFromSpring("redisService");
+    private static final ExpressionParser parser = new SpelExpressionParser();
+    private static final PrioritizedParameterNameDiscoverer parameterNameDiscoverer = new PrioritizedParameterNameDiscoverer();
+
+    //    @Autowired
 //    RedisService redisService;
 
     @Override
@@ -56,12 +62,12 @@ public class CacheInterceptor extends Perl5DynamicMethodInterceptor {
             if (returnValue == null) {
                 //log.debug("no redis cache found for [{}].[{}]!", targetClassName, methodName);
                 returnValue = methodInvocation.proceed();
+                //将结果存入Redis//异步执行
+                CacheStoreThread cacheStoreThread = new CacheStoreThread();
+                cacheStoreThread.storeResultToRedis(returnValue, redisKey, cacheKey);
             } else {
                 //log.debug("get data from redis for [{}].[{}]", targetClassName, methodName);
             }
-            //将结果存入Redis//异步执行
-            CacheStoreThread cacheStoreThread = new CacheStoreThread();
-            cacheStoreThread.storeResultToRedis(returnValue, redisKey, cacheKey);
             //log.debug("stored data to redis for [{}].[{}]", targetClassName, methodName);
         } else {
             //log.info("[{}].[{}] is not defined as Cacheable, execute method to get result directly", targetClassName, methodName);
@@ -83,24 +89,23 @@ public class CacheInterceptor extends Perl5DynamicMethodInterceptor {
 
         if (useKey) {
             try {
-                String generatedKeyCacheKey = CacheManager.getGeneratedKeyCacheKey(args, targetClassName, methodName, cacheName, cacheKeyDef);
-                if (!StringUtil.isEmpty(generatedKeyCacheKey)) {
-                    annotationGeneratedKey = CacheManager.getGenRedisKeyFromCache(generatedKeyCacheKey);
-                    if (annotationGeneratedKey == null) {
-                        ExpressionParser parser = new SpelExpressionParser();
-                        PrioritizedParameterNameDiscoverer parameterNameDiscoverer = new PrioritizedParameterNameDiscoverer();
-                        CacheEvaluationContext cacheEvaluationContext = new CacheEvaluationContext(targetObject, method, args, parameterNameDiscoverer);
-                        Expression expression = parser.parseExpression(cacheKeyDef);
-                        annotationGeneratedKey = expression.getValue(cacheEvaluationContext, String.class);
-                        CacheManager.setGenRedisKey2Cache(generatedKeyCacheKey, annotationGeneratedKey);
-                    }
-                    if (!StringUtil.isEmpty(annotationGeneratedKey)) {
-                        customizedCacheKey = true;
-                    }
-                } else {
-                    log.error("Wrong CacheKey Definition Found:[{}] for [{}].[{}]", cacheKeyDef, targetClassName, methodName);
-                    customizedCacheKey = false;
+                //String generatedKeyCacheKey = CacheManager.getGeneratedKeyCacheKey(args, targetClassName, methodName, cacheName, cacheKeyDef);
+                //if (!StringUtil.isEmpty(generatedKeyCacheKey)) {
+                //    annotationGeneratedKey = CacheManager.getGenRedisKeyFromCache(generatedKeyCacheKey);
+                //    if (annotationGeneratedKey == null) {
+                //        log.info("annotationGeneratedKey is null{}", generatedKeyCacheKey);
+                CacheEvaluationContext cacheEvaluationContext = new CacheEvaluationContext(targetObject, method, args, parameterNameDiscoverer);
+                Expression expression = parser.parseExpression(cacheKeyDef);
+                annotationGeneratedKey = expression.getValue(cacheEvaluationContext, String.class);
+                //        CacheManager.setGenRedisKey2Cache(generatedKeyCacheKey, annotationGeneratedKey);
+                //    }
+                if (!StringUtil.isEmpty(annotationGeneratedKey)) {
+                    customizedCacheKey = true;
                 }
+                //} else {
+                //    log.error("Wrong CacheKey Definition Found:[{}] for [{}].[{}]", cacheKeyDef, targetClassName, methodName);
+                //    customizedCacheKey = false;
+                //}
             } catch (Exception e) {
                 log.error("Wrong CacheKey Definition Found:[{}] for [{}].[{}]", cacheKeyDef, targetClassName, methodName, e);
                 customizedCacheKey = false;
