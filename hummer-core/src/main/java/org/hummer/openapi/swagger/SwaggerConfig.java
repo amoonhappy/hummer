69ef2562e3653ba6fa21bdfa98d7c43f8b6dc53f
amoonhappy <152318@qq.com>
2018-07-10 11:47:41 +0800

    Swagger2 Integration

---
+++ hummer-core/src/main/java/org/hummer/openapi/swagger/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package org.hummer.openapi.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@EnableWebMvc
+@ComponentScan(basePackages = {"org.hummer.openapi", "org.hummer.newweb.controller"})
+public class SwaggerConfig {
+    @Bean
+    public Docket buildDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(buildApiInf())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo buildApiInf() {
+        return new ApiInfoBuilder()
+                .title("Hummer OpenAPIs")
+                .termsOfServiceUrl("https://github.com/amoonhappy/hummer")
+                .description("Hummer Framework OpenAPIs")
+                .contact(new Contact("Jeff Zhou", "https://github.com/amoonhappy/hummer", "152318@qq.com"))
+                .build();
+
+    }
+}
